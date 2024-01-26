c67d359cc1f2c0559f7858e9e33c03e0b5677a48
Elnur Gafarzade <138590291+1907elnur@users.noreply.github.com>
2024-01-27 00:02:07 +0400

    EduHome

---
+++ EduHome/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduHome.Core.Entities.BaseEntities
+{
+	internal class BaseEntity
+	{
+		
+        public int Id { get; set; }
+		public bool IsDeleted { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+	
+	}
+}
---
+++ EduHome/EduHome.Core/Entities/Teachers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduHome.Core.Entities
+{
+	internal class Teachers
+	{
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+    }
+}
